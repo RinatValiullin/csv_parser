be5d20463d6742e90e75527f3d820f25b0f086b1
cunfox <cunningfox92@gmail.com>
2020-04-29 03:32:06 +0300

    Added specs for decorated_rows_composer

--- spec/decorators/int_decorator_spec.rb
+++ spec/decorators/int_decorator_spec.rb
@@ -5,6 +5,7 @@ require "./decorators/int_decorator.rb"
 describe IntDecorator do
   it "#decorate" do
     data = %w(1 100 10)
+
     expect(IntDecorator.new(data).decorate).to eql [["  1"], ["100"], [" 10"]]
   end
 end
